52367750d18fb8ed552316225f0a34ff5412963c
Knocker Growl <knockergrowl@users.noreply.github.com>
2023-05-14 12:38:24 +0100

    Update Nopegrams (2023-05-14T12:38+01:00)

---
+++ nopegrams/_next/static/NwOWdiVEmtcURbu3EoVG7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/505-013193b646fb842b.js","static/chunks/pages/index-cb45b7b72936999d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
